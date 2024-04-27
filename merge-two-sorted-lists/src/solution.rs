a916aeda55ea6e52dbc6859bd9083100fefa5398
lxw <xianwen.liao@newtrials.cn>
2024-04-27 23:55:08 +0800

    增加相关comments

--- merge-two-sorted-lists/src/solution.rs
+++ merge-two-sorted-lists/src/solution.rs
@@ -19,18 +19,22 @@ impl ListNode {
 impl Solution {
     pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         match (list1, list2) {
+            // 都是空的
             (None, None) => None,
+            // 其中一个是空的
             (Some(list), None) | (None, Some(list)) => Some(list),
+            // 两个都非空
             (Some(mut list1), Some(mut list2)) => {
                 if list1.val <= list2.val {
+                    // 继续拿着list1的next与list2的所有节点进行比较
                     list1.next = Solution::merge_two_lists(list1.next, Some(list2));
                     Some(list1)
                 } else {
+                    // 继续拿着list2的next与list1的所有节点进行比较
                     list2.next = Solution::merge_two_lists(Some(list1), list2.next);
                     Some(list2)
                 }
             }
         }
     }
-
 }
