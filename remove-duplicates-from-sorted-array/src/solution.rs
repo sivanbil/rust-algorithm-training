b2861040704276e9d1f5ab6f617d4a04a0defe81
lxw <xianwen.liao@newtrials.cn>
2024-05-07 08:27:32 +0800

    remove-duplicates

---
+++ remove-duplicates-from-sorted-array/src/solution.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+
+        // 双指针
+        let mut i = 0;
+        for j in 1..nums.len() {
+            if nums[i] != nums[j] {
+                i += 1;
+                nums[i] = nums[j];
+            }
+        }
+
+        (i + 1) as i32
+    }
+}
