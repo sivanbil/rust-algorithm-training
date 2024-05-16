7405d3e4c1e60fd221d25b6f21587fd05b5c65f3
lxw <xianwen.liao@newtrials.cn>
2024-05-16 08:30:06 +0800

    single-number

---
+++ single-number/src/main.rs
@@ -0,0 +1,9 @@
+mod solution;
+// https://leetcode.cn/problems/single-number/description/
+
+use crate::solution::Solution;
+
+fn main() {
+
+    println!("Hello, world!{}", Solution::single_number(vec![4, 2, 1, 2, 1]));
+}
---
+++ single-number/src/solution.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    // 异或运算有以下几个特性:
+    //
+    // 任何数与0异或等于它本身: a ^ 0 = a
+    // 任何数与它自己异或等于0: a ^ a = 0
+    // 异或运算满足交换律和结合律: a ^ b ^ a = (a ^ a) ^ b = 0 ^ b = b
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut once_number = nums[0];
+        if nums.clone().len() == 1 {
+            return once_number;
+        }
+        let mut hash_map = HashMap::new();
+        for num in nums {
+            *hash_map.entry(num).or_insert(0) ^= 1;
+        }
+        for (num, &count) in &hash_map {
+            if count == 1 {
+                return *num;
+            }
+        }
+        once_number
+    }
+
+    pub fn single_number_or(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+        for num in nums {
+            result ^=num;
+        }
+        result
+    }
+    // 题目前提非空数组nums
+    pub fn single_number_low(nums: Vec<i32>) -> i32 {
+        let mut once_number = nums[0];
+        if nums.clone().len() == 1 {
+            return once_number;
+        }
+        let mut map = HashMap::new();
+
+        for i in nums.clone() {
+            if map.contains_key(&i) {
+                let num = map.get(&i).unwrap();
+                map.insert(i, num+1);
+            } else {
+                map.insert(i, 1);
+            }
+        }
+
+        for j in  nums.clone() {
+            if map.contains_key(&j) && map.get(&j).unwrap().eq(&1) {
+                return j;
+            }
+        }
+
+        once_number
+
+    }
+}
