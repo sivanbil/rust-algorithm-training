72721b36bbea86e54e8909553dd2f41f41dbe907
lxw <xianwen.liao@newtrials.cn>
2024-05-17 21:10:59 +0800

    update md

--- majority-element/src/main.rs
+++ majority-element/src/main.rs
@@ -1,5 +1,6 @@
 mod solution;
 use crate::solution::Solution;
+// https://leetcode.cn/problems/majority-element/description/
 fn main() {
     println!("Hello, world!{}", Solution::majority_element(vec![3, 3, 2]));
 }
